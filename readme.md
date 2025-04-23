8dc5d40cd0b2fcb7e6fa1ad6265f652dfd114909
unknown <leandrovictor0103@gmail.com>
2025-04-22 21:11:53 -0300

    Adicionando abas

---
+++ readme.md
@@ -0,0 +1,13 @@
+Referencias:
+
+https://forbes.com.br/forbes-money/2024/10/cinco-crises-que-moldaram-o-mercado-de-petroleo-e-o-impacto-na-petrobras/ (Crises do petroleo ao longo do tempo)
+
+https://www.cnnbrasil.com.br/economia/macroeconomia/preco-do-petroleo-bate-maior-nivel-desde-2008-com-atrasos-em-negociacoes-no-ira/ (Alta de 2022)
+
+https://www.bbc.com/portuguese/reporterbbc/story/2008/12/081217_petroleo_qandarg (Alta e queda de 2008 a 2009)
+
+https://www.cnnbrasil.com.br/economia/macroeconomia/ha-um-ano-preco-do-petroleo-estava-negativo-mas-isso-agora-e-passado/ (Petroleo entre 2020 e 2021 - COVID)
+
+https://www.bbc.com/portuguese/reporterbbc/story/2008/09/080915_lehman_qa_pu (Crise de 2008)
+
+https://pt.wikipedia.org/wiki/Crise_financeira_de_2007%E2%80%932008 (Crise de 2008)
--- streamlit.py
+++ streamlit.py
@@ -3,89 +3,102 @@ import pandas as pd
 import plotly.express as px
 from scrapping import GetIpeaDataPetroleo
 
-st.title("Visualização e Download de Dados")
+# Criar as abas
+tab1, tab2, tab3 = st.tabs(["Storytelling e download dos dados", "Machine Learning", "Estrutura do projeto"])
 
-csv_file = "tabela_extraida.csv"
+# Aba 1: Storytelling e download dos dados
+with tab1:
+    st.title("Storytelling e download dos dados")
 
+    csv_file = "tabela_extraida.csv"
 
-try:
-    df = pd.read_csv(csv_file)
-    
-    if "Data" in df.columns and "Preço - petróleo bruto - Brent (FOB)" in df.columns:
-        # Converter a coluna "Data" para o formato datetime
-        df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y", errors="coerce")
+    try:
+        df = pd.read_csv(csv_file)
         
-        # Remover linhas com valores inválidos
-        df = df.dropna(subset=["Data", "Preço - petróleo bruto - Brent (FOB)"])
+        if "Data" in df.columns and "Preço - petróleo bruto - Brent (FOB)" in df.columns:
+            # Converter a coluna "Data" para o formato datetime
+            df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y", errors="coerce")
+            
+            # Remover linhas com valores inválidos
+            df = df.dropna(subset=["Data", "Preço - petróleo bruto - Brent (FOB)"])
+            
+            datamaxima = df["Data"].max()
+            dataminima = df["Data"].min()
+
+            st.markdown(f"""Data de: **{dataminima.strftime('%d/%m/%Y')}** ate **{datamaxima.strftime('%d/%m/%Y')}**  
+                        Total de registros: **{len(df)}**  
+                        Dados extraidos do IPEA - Instituto de Pesquisa Econômica Aplicada  
+                        Fonte: http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view  
+                        """)
+
+            st.write("------")
+
+            # Converter a coluna de preços para float
+            df["Preço - petróleo bruto - Brent (FOB)"] = df["Preço - petróleo bruto - Brent (FOB)"].str.replace(",", ".").astype(float)
+            
+            st.subheader("Gráfico Temporal")
+            fig = px.line(
+                df,
+                x="Data",
+                y="Preço - petróleo bruto - Brent (FOB)",
+                title="Evolução do Preço do Petróleo Brent",
+                labels={"Data": "Data", "Preço - petróleo bruto - Brent (FOB)": "Preço"},
+            )
+            st.plotly_chart(fig)
+        else:
+            st.error("O arquivo CSV não contém as colunas necessárias: 'Data' e 'Preço - petróleo bruto - Brent (FOB)'.")
         
-        datamaxima = df["Data"].max()
-        dataminima = df["Data"].min()
+        st.write("------") 
 
-        st.markdown(f"""Data de: **{dataminima.strftime('%d/%m/%Y')}** ate **{datamaxima.strftime('%d/%m/%Y')}**  
-                    Total de registros: **{len(df)}**  
-                    Dados extraidos do IPEA - Instituto de Pesquisa Econômica Aplicada  
-                    Fonte: http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view  
-                    """)
+        st.subheader("Dados do CSV")
+        st.write("")
+        st.dataframe(df)
 
-        st.write("------")
-
-
-        # Converter a coluna de preços para float
-        df["Preço - petróleo bruto - Brent (FOB)"] = df["Preço - petróleo bruto - Brent (FOB)"].str.replace(",", ".").astype(float)
-        
-        st.subheader("Gráfico Temporal")
-        fig = px.line(
-            df,
-            x="Data",
-            y="Preço - petróleo bruto - Brent (FOB)",
-            title="Evolução do Preço do Petróleo Brent",
-            labels={"Data": "Data", "Preço - petróleo bruto - Brent (FOB)": "Preço"},
+        st.subheader("Baixar Dados")
+        st.write("")
+        csv = df.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label="Baixar tabela_extraida.csv",
+            data=csv,
+            file_name="tabela_extraida.csv",
+            mime="text/csv",
         )
-        st.plotly_chart(fig)
-    else:
-        st.error("O arquivo CSV não contém as colunas necessárias: 'Data' e 'Preço - petróleo bruto - Brent (FOB)'.")
-    
+
+    except FileNotFoundError:
+        st.error(f"O arquivo '{csv_file}' não foi encontrado.")
+    except Exception as e:
+        st.error(f"Ocorreu um erro: {e}")
     st.write("------") 
 
-    st.subheader("Dados do CSV")
+    # Botão para executar o script scrapping.py
+    st.subheader("Atualizar Tabela")
+    st.markdown("""Clique no botão abaixo para atualizar a tabela caso esteja desatualizada.  
+                Antes de clicar cheque antes na fonte se a tabela está atualizada.
+                """) 
+
     st.write("")
-    st.dataframe(df)
 
-    st.subheader("Baixar Dados")
-    st.write("")
-    csv = df.to_csv(index=False).encode('utf-8')
-    st.download_button(
-        label="Baixar tabela_extraida.csv",
-        data=csv,
-        file_name="tabela_extraida.csv",
-        mime="text/csv",
-    )
+    if st.button("Atualizar Tabela caso esteja desatualizada"):
+        try:
+            # Executar o script scrapping.py
+            data_fetcher = GetIpeaDataPetroleo()
+            df = data_fetcher.fetch_data()
+            
+            if df is not None:
+                # Salvar o DataFrame em um arquivo CSV
+                data_fetcher.save_to_csv(df)
+                st.success(f"Tabela atualizada e salva como '{csv_file}'.")
+            else:
+                st.error("Erro ao atualizar a tabela.")
+        except Exception as e:
+            st.error(f"Ocorreu um erro ao executar o script: {e}")
 
-except FileNotFoundError:
-    st.error(f"O arquivo '{csv_file}' não foi encontrado.")
-except Exception as e:
-    st.error(f"Ocorreu um erro: {e}")
-st.write("------") 
+# Aba 2: Machine Learning (vazia)
+with tab2:
+    st.title("Machine Learning")
+    st.write("Conteúdo em desenvolvimento.")
 
-# Botão para executar o script scrapping.py
-st.subheader("Atualizar Tabela")
-st.markdown("""Clique no botão abaixo para atualizar a tabela caso esteja desatualizada.  
-            Antes de clicar cheque antes na fonte se a tabela está atualizada.
-            """) 
-
-st.write("")
-
-if st.button("Atualizar Tabela caso esteja desatualizada"):
-    try:
-        # Executar o script scrapping.py
-        data_fetcher = GetIpeaDataPetroleo()
-        df = data_fetcher.fetch_data()
-        
-        if df is not None:
-            # Salvar o DataFrame em um arquivo CSV
-            data_fetcher.save_to_csv(df)
-            st.success(f"Tabela atualizada e salva como '{csv_file}'.")
-        else:
-            st.error("Erro ao atualizar a tabela.")
-    except Exception as e:
-        st.error(f"Ocorreu um erro ao executar o script: {e}")
+# Aba 3: Estrutura do Projeto (vazia)
+with tab3:
+    st.title("Estrutura do Projeto")
+    st.write("Conteúdo em desenvolvimento.")
